cbee7d0b42e64509370e4861201ee9c397d4b3ad
BoydyBoydy <carsonboyd91@gmail.com>
2022-06-19 18:02:19 +1000

    Removed unneeded lines

--- app/src/main/kotlin/ezProject/App.kt
+++ app/src/main/kotlin/ezProject/App.kt
@@ -1,14 +1,8 @@
 package ezProject
 
 class App {
-    val greeting: String
-        get() {
-            return "Hello World!"
-        }
 
     fun main() {
-
-        println(App().greeting)
 
         val task = Task("Task")
         println(task.toString())
