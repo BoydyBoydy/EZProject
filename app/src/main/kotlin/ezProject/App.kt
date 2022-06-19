5bbcdcfd2e9f3fe4f1d87fc2fd8d727f87d4be2d
BoydyBoydy <carsonboyd91@gmail.com>
2022-06-19 22:24:10 +1000

    Updated for GUI, gradle clean up - moved main function to outside of class

--- app/src/main/kotlin/ezProject/App.kt
+++ app/src/main/kotlin/ezProject/App.kt
@@ -1,10 +1,38 @@
 package ezProject
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowState
+import androidx.compose.ui.window.application
+
 class App {
 
-    fun main() {
+    // fun main() {
 
-        val task = Task("Task")
-        println(task.toString())
+    //     val task = Task("Task")
+    //     println(task.toString())
+    // }
+}
+
+public fun main() = application {
+    Window(
+            onCloseRequest = ::exitApplication,
+            state = WindowState(width = 400.dp, height = 300.dp),
+            title = "Medium"
+    ) {
+        Box(modifier = Modifier.fillMaxWidth().fillMaxHeight()) {
+            Text(
+                    text = "Hello World",
+                    fontSize = 30.sp,
+                    modifier = Modifier.align(Alignment.Center)
+            )
+        }
     }
 }
