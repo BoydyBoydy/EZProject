46b7e006db48a72a21cc334b016702d994786800
BoydyBoydy <carsonboyd91@gmail.com>
2022-06-25 21:53:28 +1000

    Testing Decompose for Kotlin UI, as Navigation is needed

--- app/src/main/kotlin/ezProject/App.kt
+++ app/src/main/kotlin/ezProject/App.kt
@@ -9,15 +9,16 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.key.Key
-import androidx.compose.ui.input.key.KeyShortcut
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.MenuBar
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import com.arkivanov.decompose.ExperimentalDecomposeApi
+import com.arkivanov.decompose.extensions.compose.jetbrains.lifecycle.LifecycleController
+import com.arkivanov.essenty.lifecycle.LifecycleRegistry
 
 class App {
 
@@ -29,100 +30,201 @@ class App {
 
 }
 
-@OptIn(ExperimentalComposeUiApi::class)
-fun main() = application {
-    var view = View()
-    var model = Model()
-    var controller = ezProject.Controller(model, view)
-    view.projectWindow(controller)
+// @OptIn(ExperimentalComposeUiApi::class)
+@OptIn(com.arkivanov.decompose.ExperimentalDecomposeApi::class)
+fun main() {
+    val lifecycle = LifecycleRegistry()
 
-    var action by mutableStateOf("Last action: None")
+    application {
+        val windowState = rememberWindowState()
+        var action by mutableStateOf("Last action: None")
 
-    Window(
-            onCloseRequest = ::exitApplication,
-            title = "EZProject",
-            state = WindowState(width = 1300.dp, height = 750.dp)
-    ) {
-        Column() {
-            TextButton(onClick = {}, modifier = Modifier.padding(8.dp)) {
-                Text(text = "Change View")
-            }
-        } // view.setSecond()
-        MenuBar {
-            Menu("Tasks", mnemonic = 'T') {
-                Item(
-                        "Create new Task",
-                        onClick = { action = "Last action: Create new Task" },
-                        shortcut = KeyShortcut(Key.C, ctrl = true)
-                )
-            }
-            Menu("Views", mnemonic = 'V') {
-                Item(
-                        "Task List",
-                        onClick = { action = "Last action: Task List" },
-                        shortcut = KeyShortcut(Key.C, ctrl = true)
-                )
-                Item(
-                        "Gantt Chart",
-                        onClick = { action = "Last action: Gantt Chart" },
-                        shortcut = KeyShortcut(Key.V, ctrl = true)
-                )
-                Item(
-                        "Kanban Board",
-                        onClick = { action = "Last action: Kanban Board" },
-                        shortcut = KeyShortcut(Key.V, ctrl = true)
-                )
-            }
-            Menu("Resources", mnemonic = 'R') {
-                Item(
-                        "Create Resources",
-                        onClick = { action = "Last action: Create Resources" },
-                        shortcut = KeyShortcut(Key.C, ctrl = true)
-                )
-            }
-            Menu("Costs", mnemonic = 'C') {
-                Item(
-                        "Create Costs",
-                        onClick = { action = "Last action: Create Costs" },
-                        shortcut = KeyShortcut(Key.C, ctrl = true)
-                )
-            }
-            Menu("Baselines", mnemonic = 'B') {
-                Item(
-                        "Create Baseline",
-                        onClick = { action = "Last action: Create Baseline" },
-                        shortcut = KeyShortcut(Key.C, ctrl = true)
-                )
-            }
+        LifecycleController(lifecycle, windowState)
 
-            // Menu("Actions", mnemonic = 'A') {
-            //     CheckboxItem(
-            //             "Advanced settings",
-            //             checked = isSubmenuShowing,
-            //             onCheckedChange = { isSubmenuShowing = !isSubmenuShowing }
-            //     )
-            //     if (isSubmenuShowing) {
-            //         Menu("Settings") {
-            //             Item("Setting 1", onClick = { action = "Last action: Setting 1"
-            // })
-            //             Item("Setting 2", onClick = { action = "Last action: Setting 2"
-            // })
-            //         }
-            //     }
-            //     Separator()
-            //     Item("About", icon = TrayIcon, onClick = { action = "Last action: About"
-            // })
-            //     Item(
-            //             "Exit",
-            //             onClick = { isOpen = false },
-            //             shortcut = KeyShortcut(Key.Escape),
-            //             mnemonic = 'E'
-            //     )
-            // }
+        Window(
+                onCloseRequest = ::exitApplication,
+                title = "EZProject",
+                state = WindowState(width = 1300.dp, height = 750.dp)
+        ) {
+            Column() {
+                TextButton(onClick = {}, modifier = Modifier.padding(8.dp)) {
+                    Text(text = "Change View")
+                }
+            } // view.setSecond()
+            MenuBar {
+                Menu("Tasks", mnemonic = 'T') {
+                    Item(
+                            "Create new Task",
+                            onClick = { action = "Last action: Create new Task" },
+                            // shortcut = KeyShortcut(Key.C, ctrl = true)
+                            )
+                }
+                Menu("Views", mnemonic = 'V') {
+                    Item(
+                            "Task List",
+                            onClick = { action = "Last action: Task List" },
+                            // shortcut = KeyShortcut(Key.C, ctrl = true)
+                            )
+                    Item(
+                            "Gantt Chart",
+                            onClick = { action = "Last action: Gantt Chart" },
+                            // shortcut = KeyShortcut(Key.V, ctrl = true)
+                            )
+                    Item(
+                            "Kanban Board",
+                            onClick = { action = "Last action: Kanban Board" },
+                            // shortcut = KeyShortcut(Key.V, ctrl = true)
+                            )
+                }
+                Menu("Resources", mnemonic = 'R') {
+                    Item(
+                            "Create Resources",
+                            onClick = { action = "Last action: Create Resources" },
+                            // shortcut = KeyShortcut(Key.C, ctrl = true)
+                            )
+                }
+                Menu("Costs", mnemonic = 'C') {
+                    Item(
+                            "Create Costs",
+                            onClick = { action = "Last action: Create Costs" },
+                            // shortcut = KeyShortcut(Key.C, ctrl = true)
+                            )
+                }
+                Menu("Baselines", mnemonic = 'B') {
+                    Item(
+                            "Create Baseline",
+                            onClick = { action = "Last action: Create Baseline" },
+                            // shortcut = KeyShortcut(Key.C, ctrl = true)
+                            )
+                }
 
-        }
-        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-            Text(text = action)
+                // Menu("Actions", mnemonic = 'A') {
+                //     CheckboxItem(
+                //             "Advanced settings",
+                //             checked = isSubmenuShowing,
+                //             onCheckedChange = { isSubmenuShowing = !isSubmenuShowing }
+                //     )
+                //     if (isSubmenuShowing) {
+                //         Menu("Settings") {
+                //             Item("Setting 1", onClick = { action = "Last action: Setting 1"
+                // })
+                //             Item("Setting 2", onClick = { action = "Last action: Setting 2"
+                // })
+                //         }
+                //     }
+                //     Separator()
+                //     Item("About", icon = TrayIcon, onClick = { action = "Last action: About"
+                // })
+                //     Item(
+                //             "Exit",
+                //             onClick = { isOpen = false },
+                //             shortcut = KeyShortcut(Key.Escape),
+                //             mnemonic = 'E'
+                //     )
+                // }
+
+            }
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Text(text = action)
+            }
         }
     }
 }
+
+// @OptIn(ExperimentalComposeUiApi::class)
+// fun main() = application {
+//     var view = View()
+//     var model = Model()
+//     var controller = ezProject.Controller(model, view)
+//     view.projectWindow(controller)
+
+//     var action by mutableStateOf("Last action: None")
+
+    // Window(
+    //         onCloseRequest = ::exitApplication,
+    //         title = "EZProject",
+    //         state = WindowState(width = 1300.dp, height = 750.dp)
+    // ) {
+    //     Column() {
+    //         TextButton(onClick = {}, modifier = Modifier.padding(8.dp)) {
+    //             Text(text = "Change View")
+    //         }
+    //     } // view.setSecond()
+    //     MenuBar {
+    //         Menu("Tasks", mnemonic = 'T') {
+    //             Item(
+    //                     "Create new Task",
+    //                     onClick = { action = "Last action: Create new Task" },
+    //                     shortcut = KeyShortcut(Key.C, ctrl = true)
+    //             )
+    //         }
+    //         Menu("Views", mnemonic = 'V') {
+    //             Item(
+    //                     "Task List",
+    //                     onClick = { action = "Last action: Task List" },
+    //                     shortcut = KeyShortcut(Key.C, ctrl = true)
+    //             )
+    //             Item(
+    //                     "Gantt Chart",
+    //                     onClick = { action = "Last action: Gantt Chart" },
+    //                     shortcut = KeyShortcut(Key.V, ctrl = true)
+    //             )
+    //             Item(
+    //                     "Kanban Board",
+    //                     onClick = { action = "Last action: Kanban Board" },
+    //                     shortcut = KeyShortcut(Key.V, ctrl = true)
+    //             )
+    //         }
+    //         Menu("Resources", mnemonic = 'R') {
+    //             Item(
+    //                     "Create Resources",
+    //                     onClick = { action = "Last action: Create Resources" },
+    //                     shortcut = KeyShortcut(Key.C, ctrl = true)
+    //             )
+    //         }
+    //         Menu("Costs", mnemonic = 'C') {
+    //             Item(
+    //                     "Create Costs",
+    //                     onClick = { action = "Last action: Create Costs" },
+    //                     shortcut = KeyShortcut(Key.C, ctrl = true)
+    //             )
+    //         }
+    //         Menu("Baselines", mnemonic = 'B') {
+    //             Item(
+    //                     "Create Baseline",
+    //                     onClick = { action = "Last action: Create Baseline" },
+    //                     shortcut = KeyShortcut(Key.C, ctrl = true)
+    //             )
+    //         }
+
+    //         // Menu("Actions", mnemonic = 'A') {
+    //         //     CheckboxItem(
+    //         //             "Advanced settings",
+    //         //             checked = isSubmenuShowing,
+    //         //             onCheckedChange = { isSubmenuShowing = !isSubmenuShowing }
+    //         //     )
+    //         //     if (isSubmenuShowing) {
+    //         //         Menu("Settings") {
+    //         //             Item("Setting 1", onClick = { action = "Last action: Setting 1"
+    //         // })
+    //         //             Item("Setting 2", onClick = { action = "Last action: Setting 2"
+    //         // })
+    //         //         }
+    //         //     }
+    //         //     Separator()
+    //         //     Item("About", icon = TrayIcon, onClick = { action = "Last action: About"
+    //         // })
+    //         //     Item(
+    //         //             "Exit",
+    //         //             onClick = { isOpen = false },
+    //         //             shortcut = KeyShortcut(Key.Escape),
+    //         //             mnemonic = 'E'
+    //         //     )
+    //         // }
+
+    //     }
+    //     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+    //         Text(text = action)
+    //     }
+    // }
+// }
